f2a743bfaad1fda9bb6a78703646d6446ed0bcb5
kondratyevan <kondratyevan@ozon.ru>
2024-04-14 01:09:49 +0300

    Достижения создаются

--- src/achievements/service.py
+++ src/achievements/service.py
@@ -12,3 +12,7 @@ async def upload_achievement(db_conn: PoolConnectionProxy, achievement: Achievem
 async def download_achievements(db_conn: PoolConnectionProxy, user_id: int,
                                 offset: int = 0, limit: int = 100) -> list[AchievementDto]:
     return await crud.get_all(db_conn, user_id=user_id, offset=offset, limit=limit)
+
+
+async def get_achievement(db_conn: PoolConnectionProxy, achievement_id: int) -> AchievementDto:
+    return await crud.get_by_id(db_conn, achievement_id=achievement_id)
